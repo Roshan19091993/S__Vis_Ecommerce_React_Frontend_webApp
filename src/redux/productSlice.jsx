b6fcadaa584284e6ee0a930108e8e7f4b249c0f8
Roshan Borkar <roshan.borkar@suavisinc.com>
2025-11-04 23:10:55 +0530

    productSlice updated and search suggestion working

--- src/redux/productSlice.jsx
+++ src/redux/productSlice.jsx
@@ -1,30 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
 
-const initialState= {
-  products: [],
-  searchTerm: '',
-  filteredData:[]
-}
+// const initialState= {
+//   products: [],
+//   searchTerm: '',
+//   filteredData:[]
+// }
 
-const productSlice= createSlice({
-    name:"product",
-    initialState,
+// const productSlice= createSlice({
+//     name:"product",
+//     initialState,
 
    
-    reducers:{
-      setProducts:(state,action)=>{
-        state.products= action.payload;
-      },
-      setSearchTerm(state, action){
-        state.searchTerm = action.payload;
-        state.filteredData = state.products.filter(product=>
-          product.name.toLowerCase().includes(state.searchTerm.toLowerCase())
-        )
-      }
+//     reducers:{
+//       setProducts:(state,action)=>{
+//         state.products= action.payload;
+//       },
+//       setSearchTerm(state, action){
+//         state.searchTerm = action.payload;
+//         state.filteredData = state.products.filter(product=>
+//           product.name.toLowerCase().includes(state.searchTerm.toLowerCase())
+//         )
+//       }
+//     },
+// })
+
+// export const {setProducts, setSearchTerm}=productSlice.actions;
+// export default productSlice.reducer;
+
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+  filteredData: [],
+  searchTerm: "",
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+      state.filteredData = action.payload;
     },
-})
+    setSearchTerm: (state, action) => {
+      state.searchTerm = action.payload;
+      state.filteredData = state.products.filter((p) =>
+        p.name.toLowerCase().includes(action.payload.toLowerCase())
+      );
+    },
+  },
+});
 
-export const {setProducts, setSearchTerm}=productSlice.actions;
+export const { setProducts, setSearchTerm } = productSlice.actions;
 export default productSlice.reducer;
-
